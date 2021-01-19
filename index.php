2425f5a249186d8c64aed57b1d24e97bf21beb1a
Kieroth29 <kkinhuxd@gmail.com>
2021-01-19 19:19:24 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  	<meta charset="utf-8">
+	<title>R6 catalog</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+
+	<label for="L85a2">L85A2</label>
+	<br>
+	<img src="wps/L85a2.webp" id="L85a2">
+
+	<label for="AR33">AR33</label>
+	<img src="wps/AR33.webp" id="AR33">
+
+	<br>
+	<span id="typespan">TYPE</span>
+</body>
+
+</html>
